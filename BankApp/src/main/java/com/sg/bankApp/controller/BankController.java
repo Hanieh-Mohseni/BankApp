3f61d9391465127730a8cdd2d6a40b3b9c75a5f0
Hanieh <mohseni.hanieh@gmail.com>
2023-02-26 19:29:25 -0500

    Add Bank controller

---
+++ BankApp/src/main/java/com/sg/bankApp/controller/BankController.java
@@ -0,0 +1,76 @@
+package com.sg.bankApp.controller;
+
+import com.sg.bankApp.entity.Account;
+import com.sg.bankApp.entity.Transaction;
+import com.sg.bankApp.entity.User;
+import com.sg.bankApp.model.Operation;
+import com.sg.bankApp.service.BankService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class BankController {
+
+    @Autowired
+    private BankService service;
+
+    @PostMapping("/user")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User createUser(@RequestBody User user) {
+
+        return service.createUser(user);
+    }
+
+    @PostMapping("/account")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createAccount(@RequestBody Account account) {
+        service.createAccount(account);
+        return "Account is created";
+    }
+
+    @PostMapping("/operation")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String depositOrWithdraw(@RequestBody Operation operation) {
+        service.depositOrWithdraw(operation);
+        return "Done.";
+    }
+
+    @PostMapping("/transfer")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String transferMoney(@RequestBody Transaction transaction) {
+        service.transfer(transaction);
+        return "Money is transfered.";
+    }
+
+    @GetMapping("/user/{userId}")
+    public User displayUser(@PathVariable int userId) {
+        User user = service.displayUser(userId);
+
+        return user;
+    }
+
+    @GetMapping("/accounts/user/{userId}")
+    public List<Account> displayAccountsByUserId(@PathVariable int userId) {
+        List<Account> accountList = service.displayAccountsByUserId(userId);
+
+        return accountList;
+    }
+
+    @GetMapping("/account/{accountId}")
+    public Account displayAccountsDetailById(@PathVariable int accountId) {
+        return service.displayAccountDetail(accountId);
+    }
+
+    @GetMapping("/transaction/account/{accountId}")
+    public List<Transaction> displayTransactionsByAccountId(@PathVariable int accountId,
+                                                            @RequestParam int page, @RequestParam int size) {
+
+        return service.displayTransactionsByAccountId(accountId, page, size);
+    }
+
+}
