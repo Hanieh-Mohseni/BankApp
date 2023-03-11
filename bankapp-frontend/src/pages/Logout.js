ce0b1f33555c9a3b2345854e3ef3c5366b34afd3
Hanieh <mohseni.hanieh@gmail.com>
2023-03-11 17:41:17 -0500

    Add Logout changes to invisible tobar after logout

--- bankapp-frontend/src/pages/Logout.js
+++ bankapp-frontend/src/pages/Logout.js
@@ -4,7 +4,10 @@ import { useNavigate } from "react-router";
 
 const Logout = () => {
   const { loginname, setLoginname } = useContext(UserContext);
+  const {isLoggedIn,setIsLoggedIn} = useContext(UserContext);
+
   setLoginname(null);
+  setIsLoggedIn(false);
   const navigate = useNavigate();
   navigate("/login");
 
